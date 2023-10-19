372f70e08b4e1beb64782ffe99e6d525f798c80e
Anselme Massiot <mass.anselme@gmail.com>
2023-10-19 16:52:28 +0200

    feat : make createCategory methode in CategoriesFixtures to simplify the class

--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -13,23 +13,29 @@ class CategoriesFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $parent = new Categories();
-        $parent->setName('Informatique');
-        $parent->setSlug($this->slugger->slug($parent->getName())->lower());
-        $manager->persist($parent);
+        $parent = $this->createCategory('Informatique', null, $manager);
 
-        $category = new Categories();
-        $category->setName('Ordinateurs portables');
-        $category->setSlug($this->slugger->slug($category->getName())->lower());
-        $category->setParent($parent);
-        $manager->persist($category);
+        $this->createCategory('Ordinateurs portables', $parent, $manager);
+        $this->createCategory('Ecrans', $parent, $manager);
+        $this->createCategory('Souris', $parent, $manager);
 
-        $category = new Categories();
-        $category->setName('Ecran');
-        $category->setSlug($this->slugger->slug($category->getName())->lower());
-        $category->setParent($parent);
-        $manager->persist($category);
+        $parent = $this->createCategory('Mode', null, $manager);
+
+        $this->createCategory('Homme', $parent, $manager);
+        $this->createCategory('Femme', $parent, $manager);
+        $this->createCategory('Enfant', $parent, $manager);
 
         $manager->flush();
+    }
+
+    public function createCategory(string $name, Categories $parent = null, ObjectManager $manager)
+    {
+        $category = new Categories();
+        $category->setName($name);
+        $category->setSlug($this->slugger->slug($category->getName())->lower());
+        $category->setParent($parent);
+        $manager->persist($category);
+
+        return $category;
     }
 }
