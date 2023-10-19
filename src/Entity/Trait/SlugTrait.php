14b49dd42bdf1f7d2dc3e46e932780c75fd197be
Anselme Massiot <mass.anselme@gmail.com>
2023-10-19 14:41:28 +0200

    refractor: creates the trait for slug and updates files where it is used

---
+++ src/Entity/Trait/SlugTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entity\Trait;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait SlugTrait
+{
+     #[ORM\Column(length: 255)]
+     private ?string $slug = null;
+
+     public function getSlug(): ?\DateTimeImmutable
+     {
+         return $this->slug;
+     }
+ 
+     public function setSlug(\DateTimeImmutable $slug): static
+     {
+         $this->slug = $slug;
+ 
+         return $this;
+     }
+}
