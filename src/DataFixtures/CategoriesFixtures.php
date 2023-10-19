58b2c990b2cb1adb1552cacd558e7014cfbb9e6e
Anselme Massiot <mass.anselme@gmail.com>
2023-10-19 18:46:17 +0200

    feat : make fixtures for images and products and update users and categories

--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -9,20 +9,24 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CategoriesFixtures extends Fixture
 {
+    private $counter = 1;
+
     public function __construct(private SluggerInterface $slugger){}
 
     public function load(ObjectManager $manager): void
     {
         $parent = $this->createCategory('Informatique', null, $manager);
+        
         $this->createCategory('Ordinateurs portables', $parent, $manager);
         $this->createCategory('Ecrans', $parent, $manager);
         $this->createCategory('Souris', $parent, $manager);
 
         $parent = $this->createCategory('Mode', null, $manager);
+
         $this->createCategory('Homme', $parent, $manager);
         $this->createCategory('Femme', $parent, $manager);
         $this->createCategory('Enfant', $parent, $manager);
-
+                
         $manager->flush();
     }
 
@@ -33,6 +37,9 @@ class CategoriesFixtures extends Fixture
         $category->setSlug($this->slugger->slug($category->getName())->lower());
         $category->setParent($parent);
         $manager->persist($category);
+
+        $this->addReference('cat-'.$this->counter, $category);
+        $this->counter++;
 
         return $category;
     }
--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -2,15 +2,51 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Faker;
 
 class UsersFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder,
+        private SluggerInterface $slugger
+    ){}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new Users();
+        $admin->setEmail('admin@demo.fr');
+        $admin->setLastname('mass');
+        $admin->setFirstname('ans');
+        $admin->setAddress('1 rue de la roche');
+        $admin->setZipcode('35000');
+        $admin->setCity('Rennes');
+        $admin->setPassword(
+            $this->passwordEncoder->hashPassword($admin, 'admin')
+        );
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($usr = 1; $usr <= 5; $usr++){
+            $user = new Users();
+            $user->setEmail($faker->email);
+            $user->setLastname($faker->lastName);
+            $user->setFirstname($faker->firstName);
+            $user->setAddress($faker->streetAddress);
+            $user->setZipcode(str_replace(' ', '' ,$faker->postcode));
+            $user->setCity($faker->city);
+            $user->setPassword(
+                $this->passwordEncoder->hashPassword($user, 'secret')
+            );
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
